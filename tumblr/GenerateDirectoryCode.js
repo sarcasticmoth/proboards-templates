f0ab05ba7ee7763896d7c1c2c27b539ee34861b7
Vanessa Ulloa <veedotbebop@gmail.com>
2021-01-21 19:12:30 -0800

    Update GenerateDirectoryCode.js
    update and simplify

--- tumblr/GenerateDirectoryCode.js
+++ tumblr/GenerateDirectoryCode.js
@@ -1,7 +1,10 @@
 function GenerateDirectoryCode() {
   /* 
-    Conditions:
+    Values:
+    - Column L - consolidated ethnicity tag
+    - Column K - consolidated tag/class value for div property
     - Column J - Picture link [no validation for 'proper' link]
+    - Column I - nada
     - Column H - check for empty cell (means not posted)
     - Column G - if Tumblr set posted, not required
     - Column F - eye color
@@ -14,13 +17,13 @@ function GenerateDirectoryCode() {
 
   // create a new Google Doc to hold output 
   var doc_output = DocumentApp.create("Output");
-  // doc_output.getBody().appendParagraph('testing adding content.');
+  var generated_count = 0;
 
   var ui = SpreadsheetApp.getUi();
   var ss = SpreadsheetApp.getActiveSpreadsheet();
   var sheet = ss.getActiveSheet();
   var range_data = sheet.getDataRange();
-  var last_column = range_data.getLastColumn();
+  // var last_column = range_data.getLastColumn();
   var last_row = range_data.getLastRow();
   var search_range = sheet.getRange(1,1,last_row - 1, last_row - 1);
 
@@ -28,44 +31,48 @@ function GenerateDirectoryCode() {
   
   var range_values = search_range.getValues();
 
-//   A				     B    C        D       E      F           H      J
-// jay mcguiness	20s	male	white	brown	blue		Yes		HTTP://
-
-// <a href="tumblr-link">
-// <div class="grid-item 20s white male brunette blue">
-// <div class="name"> NAME </div>
-// <img src="link.png">
-// <div class="info">
-// 	20's | White | male <br>
-// 	Brown hair | blue eyes
-// </div>
-// </div>
-// </a>
-
   for(i = 1; i < last_row - 1; i++)
   {
-      // check column J
-      if(range_values[i][9])
+      /*
+      Check for:
+        A - 0 : Name
+        H - 7 : Posted 
+        J - 9 : picture link (correct link value not validated)
+        K - 10 : div class value
+        L - 11 : ethnicity
+      else:
+        skip
+      */
+      if(range_values[i][0] && !range_values[i][7] && range_values[i][9] && range_values[i][10] && range_values[i][11])
       {
-        Logger.log(range_values[i][0].toString().toUpperCase());
-        if(!range_values[i][7])
+        Logger.log((generated_count + 1).toString() + ' ' + range_values[i][0].toString().toUpperCase());
+        doc_output.getBody().appendParagraph('<a href="">');
+        doc_output.getBody().appendParagraph('<div class="grid-item ' + range_values[i][10] + '">');
+        doc_output.getBody().appendParagraph('<div class="name">' + range_values[i][0].toString().toUpperCase() + '</div>');
+        doc_output.getBody().appendParagraph('<img src="' + range_values[i][9] + '">');
+        doc_output.getBody().appendParagraph('<div class="info">');
+        doc_output.getBody().appendParagraph('\t' + format_age(range_values[i][1]) + ' | ' + format_ethnicity(range_values[i][11]) + ' | ' + format_gender(range_values[i][2]));
+        doc_output.getBody().appendParagraph('\t' + format_haircolor(range_values[i][4]) + ' | ' + range_values[i][5]);
+        doc_output.getBody().appendParagraph('</div>');
+        doc_output.getBody().appendParagraph('</div>');
+        doc_output.getBody().appendParagraph('</a>\n');
+        generated_count++;
+      }
+      else
+      {
+        if(range_values[i][7])
         {
-          if((range_values[i][0]) && (range_values[i][1]) && (range_values[i][2]) && (range_values[i][3]) && (range_values[i][4]) && (range_values[i][5]))
-          {
-            doc_output.getBody().appendParagraph('<a href="">');
-            doc_output.getBody().appendParagraph('<div class="grid-item ' + range_values[i][1] + ' ' + format_gender(range_values[i][2]) + ' ' + format_ethnicity(range_values[i][3]) + ' ' + range_values[i][4].toString().toLowerCase() + ' ' + range_values[i][5].toString().toLowerCase() + '">');
-            doc_output.getBody().appendParagraph('<div class="name">' + range_values[i][0].toString().toUpperCase() + '</div>');
-            doc_output.getBody().appendParagraph('<img src="' + range_values[i][9] + '">');
-            doc_output.getBody().appendParagraph('<div class="info">');
-            doc_output.getBody().appendParagraph('\t' + format_age(range_values[i][1]) + ' | ' + format_ethnicity(range_values[i][3]) + ' | ' + format_gender(range_values[i][2]));
-            doc_output.getBody().appendParagraph('\t' + format_haircolor(range_values[i][4]) + ' | ' + range_values[i][5]);
-            doc_output.getBody().appendParagraph('</div>');
-            doc_output.getBody().appendParagraph('</div>');
-            doc_output.getBody().appendParagraph('</a>\n');
-          }
+          Logger.log(range_values[i][0].toString().toUpperCase() + ' already posted.');
+        }
+        else
+        {
+          Logger.log('cell values missing for ' + range_values[i][0].toString().toUpperCase());
         }
       }
   }   
+
+  // count how many were generated in this run
+  doc_output.getBody().appendParagraph('\n\nGenerated Count: ' + generated_count);
 
   // send result as email
   var url = doc_output.getUrl();
